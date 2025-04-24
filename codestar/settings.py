4c7b5bb2ff5ba60fb134f399ccf0295f7b6cfd27
allaafaham <alaafahham@hotmail.com>
2025-04-24 12:46:04 +0200

    Change allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-#)ungx&gr9+dr=-$a)@ir7(t8-wxp!30jy+mvd+#x(tp0^(340
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
-# ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
